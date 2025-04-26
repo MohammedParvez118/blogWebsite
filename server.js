7a3979939131bf7f4726111e12c37177d10f78be
MohammedParvez118 <160354987+MohammedParvez118@users.noreply.github.com>
2025-04-26 15:26:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
